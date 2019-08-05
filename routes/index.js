0ce85953bf5f29f5812566ab6a715a761646d71b
gkehgl1 <gkehgl2@naver.com>
2019-08-05 19:25:01 +0900

    안녕하는 기능 넣음

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var msg = {
+  hello: '안녕'
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/about/:name', function(req, res, next) {
+  let name = req.params.name || '이름없는 아이';
+  res.render('about', { msg, title: '안뇽', name });
+});
+
+router.get('/myapi', (req,res,next) => {
+  res.json({
+    hi: req.query.hi || '안녕하지 않아..',
+    hello: 3,
+  })
+})
 
 module.exports = router;
